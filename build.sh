9b5a6bfd6a59a621da3e785416651b75fbb0a9d6
kenjones <kenjones@cisco.com>
2015-12-07 19:56:34 +0000

    [FEATURE]: Add syslog support and refactoring
    Add syslog as a service to be managed by runit. Enables Vault to
    leverage the syslog Audit Trail.
    
    Refactor the consul, runit to the base along with syslog as the
    common components.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker build -t base-alpine:3.2 -f Dockerfile.base .
+docker-compose build
+docker tag -f authstore_consul kenjones/authstore-consul
+docker tag -f authstore_vault kenjones/authstore-vault
+
